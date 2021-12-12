52dc0c297aaddbd0188969e261638e2217d1d113
Manuel <manuelgrossi03@gmail.com>
2021-12-12 16:21:18 +0100

    Navigation drawer(da finire) 1

---
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+    }
+}
